44ab20724cc992f33d9d3094c6af9d5be7127eef
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2020-03-05 15:40:10 +0900

    add route_auth.go

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
+
 package main
 
 import(
-	"fmt"
+	//"fmt"
 	"net/http"
 )
-
-// func handler(writer http.ResponseWriter,request *http.Request){
-// 	fmt.Fprintf(writer,"Hello boy, %s!",request.URL.Path[1:])
-// }
-
-// func main(){
-// 	http.HandleFunc("/",handler)
-// 	http.ListenAndServe(":8080",nil)
-// }
 
 func main(){
 	mux := http.NewServeMux()
@@ -39,12 +31,31 @@ func main(){
 	server.ListenAndServe()
 }
 
-func index(w http.ResponseWriter,r * http.Request){
-	files := []string{"templates/layout.html",
-										"templates/navbar.html",
-										"templates/index.html",}
-	templates := template.Must(template.ParseFiles(files...))
+// func index(w http.ResponseWriter,r * http.Request){
+// 	files := []string{"templates/layout.html",
+// 										"templates/navbar.html",
+// 										"templates/index.html",}
+// 	templates := template.Must(template.ParseFiles(files...))
+// 	threads, err := data.Threads(); if err == nil{
+// 		templates.ExecuteTemplate(w,"layout",threads)
+// 	}
+// }
+
+func index(w http.ResponseWriter,r *http.Request){
 	threads, err := data.Threads(); if err == nil{
+		_, err := session(w,r)
+		public_tmpl_files := []string{ "templates/layout.html",
+																	 "templates/public.navbar.html",
+																	 "templates/index.html"}
+		private_tmpl_files := []string{"templates/layout.html",
+																	"templates/private.navbar.html",
+																	"templates/index.html"}
+		var templates *template.template
+		if err != nil{
+			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+		} else{
+			templates = template.Must(template.ParseFiles(public_templ_files...))
+		}
 		templates.ExecuteTemplate(w,"layout",threads)
 	}
 }
